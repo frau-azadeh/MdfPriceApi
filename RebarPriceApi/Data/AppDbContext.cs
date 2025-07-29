0bc1522e56f1c7b025ffe163bd436e67ad481a09
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-29 16:32:48 +0330

    add : make AppDbContext

---
+++ RebarPriceApi/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RebarPriceApi.Models;
+
+namespace RebarPriceApi.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        
+        public DbSet<Product> Products { get; set; }
+    }
+}
