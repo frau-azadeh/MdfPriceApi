b352a46fb6f379e2709d03b4406a76b2abd7d6cb
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-29 16:24:15 +0330

    Add project files.

---
+++ RebarPriceApi/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace RebarPriceApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Size { get; set; }
+        public string Form { get; set; }
+        public string Standard { get; set; }
+        public string Warehouse { get; set; }
+        public string Unit { get; set; }
+        public int price { get; set; }
+        public DateTime LastPriceDate { get; set; }
+    }
+}
