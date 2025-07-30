40919b62f1840b02c3ff247cbec4f0860dbfa4b7
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-30 11:31:28 +0330

    update: migration database and use Api

--- RebarPriceApi/Migrations/20250730075758_UpdatePriceColumnToDecimal.Designer.cs
+++ RebarPriceApi/Migrations/20250730075758_UpdatePriceColumnToDecimal.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RebarPriceApi.Data;
 
@@ -11,9 +12,11 @@ using RebarPriceApi.Data;
 namespace RebarPriceApi.Migrations
 {
     [DbContext(typeof(RebarDbContext))]
-    partial class RebarDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250730075758_UpdatePriceColumnToDecimal")]
+    partial class UpdatePriceColumnToDecimal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RebarPriceApi/Migrations/20250730075758_UpdatePriceColumnToDecimal.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RebarPriceApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePriceColumnToDecimal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "price",
+                table: "Products",
+                newName: "Price");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Products",
+                newName: "price");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "price",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
